2760e624a560467e5e564e578ddd0077b6e82cab
Leptons <lepton.st@gmail.com>
2015-11-22 13:05:50 +0900

    add: spedify typing of libmincaml.s functions

--- lib/libmincaml.s
+++ lib/libmincaml.s
@@ -6,6 +6,7 @@ _HALF:	# 0.5
 # IO
 
 # ok (recv_send.s)
+# 'a -> unit
 min_caml_send32:
 	srl	$s0, $a0, 24
 	rsb	$s0
@@ -17,6 +18,7 @@ min_caml_send32:
 	jr	$ra
 
 # ok (recv_send.s)
+# unit -> 'a
 min_caml_recv32:
 	rrb	$v0
 	sll	$v0, $v0, 8
@@ -28,11 +30,13 @@ min_caml_recv32:
 	jr	$ra
 
 # ok (print.s)
+# char -> unit
 min_caml_print_char:
 	rsb	$a0
 	jr	$ra
 
 # only work when 0 <= x <= 9 (print.s)
+# int -> unit
 min_caml_print_int:
 	addi	$a0, $a0, 0x30
 	rsb	$a0
@@ -42,6 +46,7 @@ min_caml_print_int:
 # float
 
 # ok (float.s)
+# float -> float -> bool
 min_caml_fequal:
 	fslt	$s0, $a0, $a1
 	fslt	$s1, $a1, $a0
@@ -51,21 +56,25 @@ min_caml_fequal:
 	jr	$ra
 
 # ok (float.s)
+# float -> float -> bool
 min_caml_fless:
 	fslt	$v0, $a0, $a1
 	jr	$ra
 
 # ok (float.s)
+# float -> bool
 min_caml_fispos:
 	fslt	$v0, $zero, $a0
 	jr	$ra
 
 # ok (float.s)
+# float -> bool
 min_caml_fisneg:
 	fslt	$v0, $a0, $zero
 	jr	$ra
 
 # ok (float.s)
+# float -> bool
 min_caml_fiszero:
 	sll	$a0, $a0, 1
 	beq	$a0, $zero, _fiszero_true
@@ -76,43 +85,51 @@ _fiszero_true:
 	jr	$ra
 
 # ok (float.s)
+# float -> float
 min_caml_fhalf:
 	llw	$s0, (_HALF)
 	fmul	$v0, $a0, $s0
 	jr	$ra
 
 # ok (float.s)
+# float -> float
 min_caml_fsqr:
 	fmul	$v0, $a0, $a0
 	jr	$ra
 
 # ok (float.s)
+# float -> float
 min_caml_fabs:
 	sll	$v0, $a0, 1
 	srl	$v0, $v0, 1
 	jr	$ra
 
 # ok (float.s)
+# float -> float
 min_caml_fneg:
 	fneg	$v0, $a0
 	jr	$ra
 
 # ok (float.s)
+# float -> float
 min_caml_sqrt:
 	fsqrt	$v0, $a0
 	jr	$ra
 
 # ok (float.s)
+# float -> int
 min_caml_int_of_float:
 	f2i	$v0, $a0
 	jr	$ra
 
 # ok (float.s)
+# int -> float
 min_caml_float_of_int:
 	i2f	$v0, $a0
 	jr	$ra
 
 # not implemented
+# float -> float
 min_caml_floor:
 #	flr	$v0, $a0
 	jr	$ra
